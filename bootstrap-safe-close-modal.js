e196424001fc3326dc91eba132ba2b293f4058ee
Diogo Benicá <diogobenica@gmail.com>
2015-05-18 15:08:20 -0300

    Bugfix: gets message.

--- bootstrap-safe-close-modal.js
+++ bootstrap-safe-close-modal.js
@@ -3,6 +3,8 @@
 
   $.extend($.fn.modal.Constructor.prototype, {
     hide: function() {
+      var message = this.$element.data('safe-close-message');
+
       if (!this.$element.data('safe-close') || confirm(message || "If you close this window, all information will be lost. Do you want to close?")) {
         _super.apply(this, arguments);
       }
